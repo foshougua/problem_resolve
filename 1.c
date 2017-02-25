c874c61521f1dacba0b18cf744a86bcdf080c524
wangkang <1203681417@qq.com>
2017-02-25 22:35:34 +0800

    no result

--- 1.c
+++ 1.c
@@ -4,6 +4,22 @@
 	> Mail: 
 	> Created Time: 2017年02月25日 星期六 22时04分28秒
  ************************************************************************/
+//一个二维数组中，每一行，一列都从大到小排列
+//输入一个二维数组和一个整数，判断数组中
+//是否有该整数
 
 #include<stdio.h>
+#define true 1
+#define false 0
 
+int fun(int (*arr)[5],int row,int key);
+
+int main()
+{
+
+}
+
+int fun(int (*arr)[5],int row,int key)
+{
+
+}
